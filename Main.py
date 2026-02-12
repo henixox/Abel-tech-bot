d7313a87b1893860be7a92699957d3cbf4bf18ce
henixox <117751717+henixox@users.noreply.github.com>
2026-02-13 01:27:49 +0300

    Update bot messages and API token comments

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import telebot
-import os
 
-# ትክክለኛው ቶከን
+# ለአዲሱ ቦት @Abeltechmentainbot የተሰጠ ቶከን
 API_TOKEN = '8530081968:AAFg4PPaTkLkX2U8iJSJ8hwaQWd89Xkt1vw'
 ADMIN_ID = 8596054746
 
@@ -9,11 +8,11 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "ሰላም አቤል! ቦቱ አሁን በ Render ላይ በትክክል እየሰራ ነው።")
+    bot.reply_to(message, "ሰላም አቤል! @Abeltechmentainbot አሁን በትክክል መስራት ጀምሯል።")
 
 @bot.message_handler(func=lambda message: True)
 def forward_to_admin(message):
     bot.reply_to(message, "መልዕክትዎ ደርሶናል!")
-    bot.send_message(ADMIN_ID, f"መልዕክት ከ: @{message.from_user.username}\n\n{message.text}")
+    bot.send_message(ADMIN_ID, f"አዲስ መልዕክት አለ:\n\n{message.text}")
 
 bot.infinity_polling()
